b463e435b56646874ad536b03e1e8584b00d9dc9
sarathsreedhar97 <sarathsreedhar@cet.ac.in>
2017-04-11 17:30:04 +0530

    first commit

---
+++ curl.sh
@@ -0,0 +1,11 @@
+echo "\n"
+curl http://www.google.com
+echo "\n"
+curl http://www.google.com > test.html
+ls
+cat test.html
+curl -O https://www.google.com
+curl -O https://en.wikipedia.org/wiki/Curl -O https://en.wikipedia.org/wiki/Wget
+curl -i www.google.com
+curl -L http://www.google.com
+curl -C- -O http://www.gnu.org/software/gettext/manual/gettext.html
